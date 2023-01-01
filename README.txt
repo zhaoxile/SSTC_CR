cee68dc764515305e329b4be14a06f509a336137
zhaoxile <59695675+zhaoxile@users.noreply.github.com>
2023-01-01 20:38:48 +0800

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,23 @@
+***************************************************************************
+     Spatial-Spectral-Temporal Connective Tensor Network Decomposition
+                        for Thick Cloud Removal
+***************************************************************************
+
+     Copyright:  Wen-Jie Zheng, Xi-Le Zhao*, Yu-Bang Zheng, Jie Lin, 
+                       Lina Zhuang, and Ting-Zhu Huang
+
+
+ 1). Get Started
+
+ Run the 'demo_SSTC_CR_ac.m' to run the SSTC-CR method with accurate cloud masks.
+ Run the 'demo_SSTC_CR_inac.m' to run the SSTC-CR method with inaccurate cloud masks.
+
+ 2). Citations
+
+    The dataset is available at: https://patrickTUM.github.io/cloud_removal [1]. 
+    Please cite [1] if you use this dataset.
+
+    [1] Ebel, P., Xu, Y., Schmitt, M., and Zhu, X. X., 2022. SEN12MS-CR-TS:
+        A Remote-Sensing Data Set for Multimodal Multitemporal Cloud Removal.
+        IEEE Transactions on Geoscience and Remote Sensing, 60, 1-14.
+
---
+++ demo_SSTC_CR_ac.m
@@ -0,0 +1,87 @@
+%% =================================================================
+clc;
+clear;
+close all;
+
+addpath(genpath('Data'));
+addpath(genpath('Lib'));
+addpath(genpath('quality_assess'));
+
+EN_SSTC_CR         = 1;
+
+%%
+methodname    = {'Observed','SSTC_CR'};
+Mnum          = length(methodname);
+Re_tensor     = cell(Mnum,1);
+psnr          = cell(Mnum,1);
+ssim          = cell(Mnum,1);
+sam           = cell(Mnum,1);
+nrmse         = cell(Mnum,1);
+time          = zeros(Mnum,1);
+
+%% Load initial data
+load('africa_ROIs1970_21_15_ac.mat');
+Y     = Clean;
+for i=1:3
+    Y(:,:,:,i)=Y(:,:,:,i).*Mask(:,:,i)+1-Mask(:,:,i);
+end
+
+%%
+i  = 1;
+Re_tensor{i} = Y;
+[psnr_0_1,ssim_0_1,sam_0_1] = HSIQA(255*Y(:,:,:,1),255*Clean(:,:,:,1));
+[psnr_0_2,ssim_0_2,sam_0_2] = HSIQA(255*Y(:,:,:,2),255*Clean(:,:,:,2));
+[psnr_0_3,ssim_0_3,sam_0_3] = HSIQA(255*Y(:,:,:,3),255*Clean(:,:,:,3));
+nrmse_0 = sqrt(norm(Y(:)-Clean(:))^2/numel(Y));
+psnr{i} = [psnr_0_1,psnr_0_2,psnr_0_3];
+ssim{i} = [ssim_0_1,ssim_0_2,ssim_0_3];
+sam{i}  = [sam_0_1,sam_0_2,sam_0_3];
+nrmse{i}= nrmse_0;
+enList = 1;
+
+%% Perform  algorithms
+
+%%
+i = i+1;
+if EN_SSTC_CR
+    opts=[];
+    opts.alpha      = 5*1e-3;
+    opts.beta       = 1e-3;
+    opts.rho        = 0.01;
+    opts.k_subspace = 6;
+    opts.tol        = 1e-4;
+    opts.maxit      = 1000;
+    opts.R          = [0,   1, 1, 1;
+                       0,   0, 1, 1;
+                       0,   0, 0, 1;
+                       0,   0, 0, 0];
+    opts.max_R      = [0,  200, 3, 3;
+                       0,   0,  3, 3;
+                       0,   0,  0, 2;   
+                       0,   0,  0, 0];
+    fprintf('\n');
+    disp(['performing ',methodname{i}, ' ... ']);
+    t0= tic;
+    [SSTC, Out]     = SSTC_CR(Y, Mask, opts);
+    time(i)         = toc(t0);
+    Re_tensor{i}    = SSTC;
+    [psnr_SSTC_1,ssim_SSTC_1,sam_SSTC_1] = HSIQA(255*SSTC(:,:,:,1),255*Clean(:,:,:,1));
+    [psnr_SSTC_2,ssim_SSTC_2,sam_SSTC_2] = HSIQA(255*SSTC(:,:,:,2),255*Clean(:,:,:,2));
+    [psnr_SSTC_3,ssim_SSTC_3,sam_SSTC_3] = HSIQA(255*SSTC(:,:,:,3),255*Clean(:,:,:,3));
+    nrmse_SSTC = sqrt(norm(SSTC(:)-Clean(:))^2/numel(SSTC));
+    psnr{i} = [psnr_SSTC_1,psnr_SSTC_2,psnr_SSTC_3];
+    ssim{i} = [ssim_SSTC_1,ssim_SSTC_2,ssim_SSTC_3];
+    sam{i}  = [sam_SSTC_1,sam_SSTC_2,sam_SSTC_3];
+    nrmse{i}= nrmse_SSTC;
+    enList  = [enList,i];
+end
+
+%% Show result
+fprintf('\n');
+fprintf('======================= Result ==========================\n');
+fprintf(' %8.8s    %6.6s    %6.6s    %6.6s    %6.6s  \n','method','PSNR', 'SSIM','SAM','RMSE' );
+for i = 1:length(enList)
+    fprintf(' %8.8s    %5.4f    %5.4f    %5.4f    %5.4f\n',...
+        methodname{enList(i)},mean(psnr{enList(i)}), mean(ssim{enList(i)}), mean(sam{enList(i)}), nrmse{enList(i)} );
+end
+fprintf('======================= Result ==========================\n');
